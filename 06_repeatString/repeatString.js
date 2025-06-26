95b779d6b1810424357a02309c57705237eb2d8c
Br0cod3 <thespammingbott@gmail.com>
2025-06-27 00:05:40 +0100

    passed the tests for the 6th and 7th challenge

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let concatenated_str = ""
+    if (num < 0) return "ERROR";
+    for (let i = 0; i < num; i++) {
+        concatenated_str += string
+    }
+    return concatenated_str
 };
+
+repeatString("hey", 8)
 
 // Do not edit below this line
 module.exports = repeatString;
