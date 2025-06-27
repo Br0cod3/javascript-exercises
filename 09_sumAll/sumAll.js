c42508c139a9e364e0037a2dde5b1f9e94101e04
Br0cod3 <thespammingbott@gmail.com>
2025-06-27 19:16:55 +0100

    Done with the js exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(intOne, intTwo) {
+    if (intOne < 0 || intTwo < 0) return "ERROR";
+    if (!Number.isInteger(intOne) || !Number.isInteger(intTwo)) return "ERROR";
+    let sum = 0;
+    let firstNum = null;
+    intOne < intTwo ? firstNum = intOne : firstNum = intTwo ;
+    for (let x = 0; x < Math.abs(intTwo - intOne) + 1; x++) {
+        sum += firstNum;
+        firstNum++;
+    }
+    return sum
 };
 
 // Do not edit below this line
