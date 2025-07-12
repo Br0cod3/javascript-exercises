3d7aa0883ca52a15c52cb5631fa3a60809782af6
Br0cod3 <thespammingbott@gmail.com>
2025-07-12 17:46:30 +0100

    done with the palindrome exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let newStr = str.replace(/[^a-zA-Z0-9]/g, "");
+  let modStr = newStr.split("").reverse().join("");
+  if (newStr.toLowerCase() === modStr.toLowerCase()) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
