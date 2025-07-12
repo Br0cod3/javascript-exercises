e1c2f939a4c896cab823f4b1af17db3f5a832d37
Br0cod3 <thespammingbott@gmail.com>
2025-07-12 15:44:19 +0100

    passed all tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  for (const elements of arr) {
+    sum += elements;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let multiple = 1;
+  for (const element of arr) {
+    multiple *= element;
+  }
+  return multiple;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let n = 1;
+  if (a === 0 || a === 1) {
+    return 1;
+  } else {
+    for (let i = a; i > 0; i--) {
+      n *= i;
+    }
+    return n;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
