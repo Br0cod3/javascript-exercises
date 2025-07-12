cc9ceb5d4365f632944197f40e55fc07d73cc4d0
Br0cod3 <thespammingbott@gmail.com>
2025-07-12 17:49:10 +0100

    I literally have nothing to say

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,10 +1,7 @@
 const palindromes = function (str) {
   let newStr = str.replace(/[^a-zA-Z0-9]/g, "");
   let modStr = newStr.split("").reverse().join("");
-  if (newStr.toLowerCase() === modStr.toLowerCase()) {
-    return true;
-  }
-  return false;
+  return newStr.toLowerCase() === modStr.toLowerCase()
 };
 
 // Do not edit below this line
