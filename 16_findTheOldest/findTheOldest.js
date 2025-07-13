c32ab0a5d17451ae00e59195ef92a717480d6849
Br0cod3 <thespammingbott@gmail.com>
2025-07-13 06:47:05 +0100

    also done with this exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  arr.forEach((element) => {
+    if (element.yearOfDeath === undefined) {
+      const date = new Date();
+      element.yearOfDeath = parseInt(date.getFullYear());
+    }
+  });
+  let max = arr.sort((a, b) => {
+    return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+  });
+  return max[max.length - 1]
 
 };
 
